742a4b77d594df1b6391f6911e32f6744e036218
Zhaojiawei <2920055606@qq.comdefaults write com.apple.finder AppleShowAllFiles Yes && killall Finder>
2017-11-04 15:54:13 +0800

    换位置2

---
+++ docs/ts/scripts/app.ts
@@ -0,0 +1,42 @@
+/// <reference path="../../library/jq/jqwidgets-ts/jqwidgets.d.ts" />
+/// <reference path="./model/FMShop.ts" />
+/// / <reference path="../decalaration/model/FMShop.d.ts" />
+// import *as FMShop from './model/FMShop';
+// const FMShop = require('./model/FMShop');
+// import FMShop = require('./model/FMShop');
+
+
+
+function createBulletChart(selector) {
+    var width = 500;
+    var item: FMShop.Item = new FMShop.Item();
+    // var item: any = {};
+    // Initialization options - validated in typescript
+    // jqwidgets.BulletChartOptions has generated TS definition
+    var options: jqwidgets.BulletChartOptions =
+    {
+        width: width,
+        height: 80,
+        barSize: "40%",
+        title: "Revenue 2014 YTD",
+        description: "(U.S. $ in thousands)",
+        ranges: [
+            { startValue: 0, endValue: 200, opacity: 0.5 },
+            { startValue: 200, endValue: 250, opacity: 0.3 },
+            { startValue: 250, endValue: 300, opacity: 0.1 }
+        ],
+        pointer: { value: 270, label: item.name + "Revenue 2014 YTD", size: 8, color: "Black" },
+        target: { value: 260, label: "Revenue 2013 YTD", size: 4, color: "Black" },
+        ticks: { position: "both", interval: 50, size: 10 },
+        labelsFormat: "c",
+        showTooltip: true
+    };
+    // Creates an instance
+    var myBulletChart: jqwidgets.jqxBulletChart = jqwidgets.createInstance(selector, 'jqxBulletChart', options);
+    // Event binding
+    myBulletChart.addEventHandler('change', function () {
+        // Do Something
+    });
+    // Method 'val' - set new value
+    myBulletChart.val(150);
+}
---
+++ docs/ts/scripts/model/FMShop.ts
@@ -0,0 +1,13 @@
+namespace FMShop {
+    export class Item {
+        
+        public get name() : string {
+            return this._name;
+        }
+        
+        private _name: string = "test!";
+
+
+    }
+}
+// export = FMShop;

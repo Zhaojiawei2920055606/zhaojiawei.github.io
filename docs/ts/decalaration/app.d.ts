44d433642ff473c72def700008b02cf3b48cf675
Zhaojiawei <2920055606@qq.comdefaults write com.apple.finder AppleShowAllFiles Yes && killall Finder>
2017-11-04 15:53:26 +0800

    换位置1

--- docs/ts/decalaration/app.d.ts
+++
@@ -1,1 +0,0 @@
-/// <reference path="../../library/jq/jqwidgets-ts/jqwidgets.d.ts" />
--- docs/ts/decalaration/model/FMShop.d.ts
+++
@@ -1,7 +0,0 @@
-declare module FMShop {
-    class Item {
-        readonly name: string;
-        private _name;
-    }
-}
-export = FMShop;
